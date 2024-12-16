16f92c315799856caf6c9ea33e5b6fbf2f96e991
unknown <kaleemikennels@gmail.com>
2024-12-16 16:11:19 +0500

    new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  { path: 'home', loadChildren: () => import('./components/landing-page/landing-page.module').then(m => m.LandingPageModule) },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
