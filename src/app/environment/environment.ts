3b4df3103fceb8a4a7963bf2ffa62a68bf73826c
Muhammadluqman340 <luqman.dagai@gmail.com>
2025-02-25 14:30:10 +0500

    set the main page

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,23 +1,22 @@
-// export const environment = {
-//     production: false,
-//     name: "{DEV}",
-//     apiUrl: 'https://api.patronworks.net/api/v1',
-//     socketUrl : 'https://api.patronworks.net',
-//     stripeClientId:'ca_NTuk60cLep4m855LRtD8jL3Hasxtssjp',
-//     stripeKey:'pk_live_51MiZTVF1YkHoz4Y5sBZLklUnnkRrmCZ0yfEeWnPUAluKFbvMEGPjCl7u7VEHlnQLT2PcBa3zviwQMr6ihIsQhjR900ZTqbI4eb',
-//     userId:'66fc5fb8aef1d315dc9fd4e6' //soso id
-//   };
-
-
+export const environment = {
+    production: false,
+    name: "{DEV}",
+    apiUrl: 'https://api.patronworks.net/api/v1',
+    socketUrl : 'https://api.patronworks.net',
+    stripeClientId:'ca_NTuk60cLep4m855LRtD8jL3Hasxtssjp',
+    stripeKey:'pk_live_51MiZTVF1YkHoz4Y5sBZLklUnnkRrmCZ0yfEeWnPUAluKFbvMEGPjCl7u7VEHlnQLT2PcBa3zviwQMr6ihIsQhjR900ZTqbI4eb',
+    userId:'66fc5fb8aef1d315dc9fd4e6' //soso id
+  };
   
 
   // for test mode of saad account
-export const environment = {
-  production: false,
-  name: "{DEV}",
-  apiUrl: 'http://localhost:4444/api/v1',
-  stripeClientId:'ca_NTukWuBc2ANm1HJhlCRXDECbUO7lpJUf',
-  socketUrl :'http://localhost:4444',
-  stripeKey:'pk_test_51MiZTVF1YkHoz4Y5Ei8O9pdjtQSmsgQGjBeEo2PIH17PfnUfPNMPAKaZ9K6EgThTVfNDUHwTIKJSsjBKtgwppyH800J52HWBAi',
-  userId:'65d6e2acf4cb2c368afded71'
-};
+// export const environment = {
+//   production: false,
+//   name: "{DEV}",
+//   apiUrl: 'http://localhost:4444/api/v1',
+//   stripeClientId:'ca_NTukWuBc2ANm1HJhlCRXDECbUO7lpJUf',
+//   socketUrl :'http://localhost:4444',
+//   stripeKey:'pk_test_51MiZTVF1YkHoz4Y5Ei8O9pdjtQSmsgQGjBeEo2PIH17PfnUfPNMPAKaZ9K6EgThTVfNDUHwTIKJSsjBKtgwppyH800J52HWBAi',
+//   userId:'65d6e2acf4cb2c368afded71'
+ 
+// };
